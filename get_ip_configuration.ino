0207c47336a16542738c1684ca8cc38b2321b95d
jun-a <juna.umt@gmail.com>
2026-02-20 08:46:06 +0700

    Create get_ip_configuration.ino

---
+++ get_ip_configuration.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "IDF_WirelessTAB";
+const char* password = "cikupawifi123";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("=== NETWORK INFO ===");
+  Serial.print("IP Address  : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Gateway     : ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("Subnet Mask : ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("DNS         : ");
+  Serial.println(WiFi.dnsIP());
+}
+
+void loop() {}
